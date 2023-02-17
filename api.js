374efae9ff568f7e845fc7392e05f6e6cdb75859
강혁주 <fromtoptobottom@ganghyeogjuui-MacBookAir.local>
2023-02-17 16:39:13 +0900

    [ADD] 유저 회원가입 엔드포인트 구현

---
+++ api.js
@@ -0,0 +1,61 @@
+const users = [
+  {
+    id: 1,
+    name: "Rebekah Johnson",
+    email: "Glover12345@gmail.com",
+    password: "123qwe",
+  },
+  {
+    id: 2,
+    name: "Fabian Predovic",
+    email: "Connell29@gmail.com",
+    password: "password",
+  },
+];
+
+const posts = [
+  {
+    id: 1,
+    title: "간단한 HTTP API 개발 시작!",
+    content: "Node.js에 내장되어 있는 http 모듈을 사용해서 HTTP server를 구현.",
+    userId: 1,
+  },
+  {
+    id: 2,
+    title: "HTTP의 특성",
+    content: "Request/Response와 Stateless!!",
+    userId: 2,
+  },
+];
+
+const http = require("http");
+
+const server = http.createServer();
+
+const simpleApi = (request, response) => {
+  if (request.method === "POST" && request.url === "/users") {
+    let body = "";
+    request.on("data", (data) => {
+      body += data;
+    });
+    request.on("end", () => {
+      const user = JSON.parse(body);
+
+      users.push({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      });
+
+      response.writeHead(200, { "Content-Type": "application/json" }); // (4)
+      response.end(JSON.stringify({ users: users }));
+    });
+  }
+};
+
+server.on("request", simpleApi);
+
+server.listen(8000, "127.0.0.1", function () {
+  console.log("Listening to request on port 8000");
+});
